457b35435581540bc1977e0a60a6e2b8902a9fa3
Omar Rokiba <orokiba@sundevils.asu.edu>
2021-12-12 22:54:18 +0200

    Add project files.

---
+++ Cobone/Pages/Counter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cobone.Pages
+{
+    public partial class Counter
+    {
+        [Parameter] public string Id { get; set; }
+
+    }
+}
